65f8f8ee1bf14ac0f9aa9b2b7cea9de69b21c804
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-05-11 17:47:21 +0300

    add scroll to top js

--- src/js/components/refs.js
+++ src/js/components/refs.js
@@ -1,4 +1,6 @@
 export const refs = {
     booksHandler: document.querySelector('.books__handler'),
-    categoryTitle: document.querySelector('.books__title')
+    categoryTitle: document.querySelector('.books__title'),
+    scrollTopBtn: document.querySelector('.btn-top'),
+    scrollTopTarget: document.querySelector('#top-target')
 };
