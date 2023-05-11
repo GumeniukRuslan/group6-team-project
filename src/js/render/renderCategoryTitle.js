eeb0ec26edf6b90b435ac8a729c5af35ca9819ed
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-05-11 13:41:12 +0300

    update category title rendering

--- src/js/render/renderCategoryTitle.js
+++ src/js/render/renderCategoryTitle.js
@@ -1,7 +1,9 @@
+import { refs } from "../components/refs";
+
 export const renderCategoryTitle = (title) => {
     const arrayOfWords = title.split(" ");
     const lastWord = arrayOfWords[arrayOfWords.length - 1];
     const allExceptLastWord = arrayOfWords.slice(0, -1).join(" ");
-    console.log(arrayOfWords, lastWord, allExceptLastWord);
-    return `${allExceptLastWord} <span class="books__title-last">${lastWord}</span>`
+
+    refs.categoryTitle.innerHTML = `${allExceptLastWord} <span class="books__title-last">${lastWord}</span>`;
 }
--- src/js/topBooks.js
+++ src/js/topBooks.js
@@ -1,10 +1,12 @@
 import { getTopBooks } from "./api/fetchLogic";
 import { refs } from "./components/refs";
-import { renderTopCategories } from "./render/renderTopCategories";
+import { renderTopCategories} from "./render/renderTopCategories";
+import { renderCategoryTitle } from "./render/renderCategoryTitle";
 
 const createTopBooks = async () => {
     const books = await getTopBooks();
     refs.booksHandler.innerHTML = renderTopCategories(books);
+    renderCategoryTitle(`Best sellers books`);
 }
 
 if (refs.booksHandler) {
