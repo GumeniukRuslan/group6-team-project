59a2d63b7977ae9e07698f4b386c461ff7247581
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-05-12 23:20:18 +0200

    js update

--- src/js/render/renderAllCategories.js
+++ src/js/render/renderAllCategories.js
@@ -1,15 +1,56 @@
 import { getCategoryList } from '../api/fetchLogic';
 import { refs } from '../components/refs';
 
-function renderAllCategories(data) {}
+const renderAllCategories = (data) => {
+  const categories = data.map(({ list_name }) => {
+    return `
+      <div class="categories">
+        <ul class="categories__list">
+          <li class="categories__item">All categories</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+          <li class="categories__item">${list_name}</li>
+        </ul>
+      </div>
+    `;
+  }).join("");
+  return categories;
+} 
 
 async function createAllCategories() {
   try {
-    const allCategories = '';
+    const allCategories = await getCategoryList();
+    const categoriesHtml = renderAllCategories(allCategories);
+    refs.allCategoriesList.insertAdjacentHTML("beforeend", categoriesHtml);
   } catch (error) {
     console.log(error);
   }
 }
+
+createAllCategories();
+
+// Сделал, как ты написал, но не работает.
+
+// import { getCategoryList } from '../api/fetchLogic';
+// import { refs } from '../components/refs';
+
+// function renderAllCategories(data) {}
+
+// async function createAllCategories() {
+//   try {
+//     const allCategories = '';
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 // во-первых тебе не нужна переменная с массивом объектов, ты получаешь его через функцию которую я тебе импортировал вверху, это ассинхронная функция
 // во-вторых вот это const ul = document.querySelector('.allCategories__list'); должно быть в файле refs который тоже надо импортировать, уже сделал,
