08e622d5de287505d3ea8b5a72dab50f49ab8abc
VVervolk <113511730+VVervolk@users.noreply.github.com>
2023-05-12 17:06:06 +0300

    add auth, header`s elms render on auth, complete mobile menu`s render
    build: header`s elms on auth, start mobile menu`s
    style: header`s elms on auth, start mobile menu`s
    feature: reg, auth, logIn, render elms on auth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-
 import './js/components/refs';
 import './js/mobileMenu';
 import './js/topBooks';
 import './js/scrollToTop';
+import './js/firebase';
+import './js/render/renderOnAuth';
--- src/js/components/refs.js
+++ src/js/components/refs.js
@@ -1,10 +1,19 @@
 export const refs = {
+  //Мобильное меню
   mobileMenu: document.querySelector('.js-mobileMenu'),
   burger: document.querySelector('.js-burger'),
   burgerCloseIcon: document.querySelector('.js-burgerClose'),
   burgerMenuIcon: document.querySelector('.js-burgerMenu'),
+  //
   booksHandler: document.querySelector('.books__handler'),
   categoryTitle: document.querySelector('.books__title'),
   scrollTopBtn: document.querySelector('.btn-top'),
-  scrollTopTarget: document.querySelector('#top-target')
+  scrollTopTarget: document.querySelector('#top-target'),
+  //Авторизация
+  regForm: document.querySelector('.js-regForm'),
+  logOutButton: document.querySelector('.js-logOutButton'),
+  //Header`s elements
+
+  elmsNonAuth: document.querySelectorAll('.js-elmsNonAuth'),
+  elmsAuth: document.querySelectorAll('.js-elmsAuth'),
 };
