1cd142fdb095bac384119a9593aee50128e3415c
GumeniukRuslan <bishob1997@gmail.com>
2023-05-13 11:05:21 +0300

    fix header

---
+++ src/js/helpers/formValuesGet.js
@@ -0,0 +1,12 @@
+export default function formValuesGet(formSubmitted) {
+  const form = new FormData(formSubmitted);
+  const keys = form.keys();
+  const data = {};
+  let key = keys.next().value;
+
+  while (form.has(key)) {
+    data[key] = form.get(key);
+    key = keys.next().value;
+  }
+  return data;
+}
