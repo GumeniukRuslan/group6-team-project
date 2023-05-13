094b6df35bee06b3828ff563c0a4776110d4d348
ArtemVorobiov <artz17@rambler.ru>
2023-05-13 10:00:15 +0200

    add renders

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './js/components/refs';
 import './js/mobileMenu';
 import './js/topBooks';
 import './js/scrollToTop';
+import './js/components/support-funds';
--- src/js/components/support-funds.js
+++ src/js/components/support-funds.js
@@ -2,46 +2,74 @@ export const funds = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: null,
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: null,
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: null,
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: null,
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: null,
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: null,
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: null,
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: null,
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: null,
   },
 ];
+
+const imgSupportUrl = [
+  '../../images/saveTheChildren.png',
+  './images/projectHope.png',
+  './images/united24.png',
+  './images/medicalCorps.png',
+  './images/msf.png',
+  './images/razom.png',
+  './images/actionAgainst.png',
+  './images/worldVision.png',
+  './images/prytula.png',
+];
+
+funds.forEach((fund, index) => {
+  fund.img = new URL(imgSupportUrl[index], import.meta.url);
+});
+
+const fundsListEl = document.getElementById('support-list');
+
+funds.forEach((fund) => {
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  const image = document.createElement('img');
+  
+  link.href = fund.url;
+  link.target = '_blank';
+  link.rel = 'noopener noreferrer';
+  image.src = fund.img;
+
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  fundsListEl.appendChild(listItem);
+
+});
+console.log(fundsListEl)
