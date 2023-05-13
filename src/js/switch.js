6b6c93c720754169e5c473a87a08f887a30eb4b3
KiosaOleg <117593116+KiosaOleg@users.noreply.github.com>
2023-05-13 17:44:33 +0200

    switch
    Первый пул.
    
    Немного изменил root , после того как все страницы будут доделаны поиграюсь с цветами и будет все работать

---
+++ src/js/switch.js
@@ -0,0 +1,39 @@
+let checkbox = document.querySelector('input[name=theme_switch]');
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  checkbox.checked = true;
+} else {
+  document.documentElement.setAttribute('data-theme', 'light');
+  checkbox.checked = false;
+}
+
+// switch theme if checkbox is engaged
+checkbox.addEventListener('change', cb => {
+  document.documentElement.setAttribute(
+    'data-theme',
+    cb.target.checked ? 'dark' : 'light'
+  );
+});
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  checkbox.checked = true;
+} else {
+  document.documentElement.setAttribute('data-theme', 'light');
+  checkbox.checked = false;
+}
+
+// switch theme if checkbox is engaged
+checkbox.addEventListener('change', cb => {
+  const selectedTheme = cb.target.checked ? 'dark' : 'light';
+  document.documentElement.setAttribute('data-theme', selectedTheme);
+  localStorage.setItem('theme', selectedTheme);
+});
+
+// проверяем сохраненную тему и устанавливаем ее при загрузке страницы
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  document.documentElement.setAttribute('data-theme', savedTheme);
+  checkbox.checked = savedTheme === 'dark';
+}
