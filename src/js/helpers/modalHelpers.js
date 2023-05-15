89709caf2e39dd515ed8d16b120fd087ee6d734c
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-05-15 18:45:03 +0300

    add team photos

---
+++ src/js/helpers/modalHelpers.js
@@ -0,0 +1,23 @@
+import { refs } from "../components/refs";
+
+export const cleanModal = () => {
+    refs.backdrop.innerHTML = '';
+}
+
+export const alreadyInShopList = () => {
+    return `<button class="btn--violet modal-book__btn" type="button" data-action="delete">remove from the shopping list</button>
+    <p class="modal-book__notification">
+      Congratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.
+    </p>`
+}
+
+export const notInShopList = () => {
+    return '<button class="btn--violet modal-book__btn" type="button" data-action="add">add to shopping list</button>';
+}
+
+export const unloggedUserShopList = () => {
+    return `<button class="btn--violet modal-book__btn" type="button" disabled>add to shopping list</button>
+    <p class="modal-book__notification">
+     You need to sign in to be able to add books to your shopping list.
+    </p>`;
+}
