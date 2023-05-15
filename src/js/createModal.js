4e173c0b6273aad7c786cdcd9b5b2a2114b45a40
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-05-15 19:58:59 +0300

    update open modal

--- src/js/createModal.js
+++ src/js/createModal.js
@@ -5,12 +5,40 @@ import { removeLoading, startLoading } from './helpers/spinner';
 import { renderError } from './render/renderError';
 import { closeModal } from './components/closeModalBtn';
 import { onKeyDown } from './components/closeModalOnEsc';
+import { closeModalBackdrop } from './components/closeModalBackdrop';
 
 import { addBookToShopList } from './helpers/addBookToShopList';
-import { deleteBookFromModal } from './helpers/deleteBookFromModal';
 
 import { alreadyInShopList, cleanModal, notInShopList, unloggedUserShopList } from './helpers/modalHelpers';
 import { getBksFrmShpLst, userCurrent} from './firebase';
+
+export const openModalBook = async (id) => {
+  const dataBook = await getOneBookById(id);
+  refs.backdrop.innerHTML = renderModal(dataBook);
+
+  const btns = document.querySelector('.modal-book__btns');
+  const booksInShopList = await getBksFrmShpLst();
+  if (!userCurrent) {
+      btns.innerHTML = unloggedUserShopList();
+  } else {
+      if (booksInShopList.includes(id)) {
+          btns.innerHTML = alreadyInShopList();
+      } else {
+          btns.innerHTML = notInShopList();
+      }
+  }
+  const btnClose = document.querySelector('[data-modal-close]');
+  btnClose.addEventListener('click', closeModal);
+
+  refs.backdrop.addEventListener("click", closeModalBackdrop);
+  document.body.classList.add('lock');
+  window.addEventListener('keydown', onKeyDown);
+
+  const btnShop = document.querySelector('.modal-book__btn');
+  if (btnShop.disabled = 'true') {
+    btnShop.addEventListener("click", addBookToShopList);
+  }
+}
 
 const createModal = async evt => {
   if (!evt.target.closest('.card')) {
@@ -21,30 +49,16 @@ const createModal = async evt => {
   refs.backdrop.classList.remove('is-hidden');
   const bookId = evt.target.closest('.card').dataset.book;
   try {
-    const dataBook = await getOneBookById(bookId);
-    refs.backdrop.innerHTML = renderModal(dataBook);
-
-    const btns = document.querySelector('.modal-book__btns');
-    const booksInShopList = await getBksFrmShpLst();
-    if (!userCurrent) {
-      btns.innerHTML = unloggedUserShopList();
-    } else {
-      if (booksInShopList.includes(bookId)) {
-        btns.innerHTML = alreadyInShopList();
-      } else {
-        btns.innerHTML = notInShopList();
-      }
-    }
+    openModalBook(bookId)
   } catch (e) {
     refs.backdrop.innerHTML = renderError(
       `Something went wrong`
     );
   }
-  // refs.backdrop.addEventListener('click', addBookToShopList);
-  // refs.backdrop.addEventListener('click', deleteBookFromModal);
-  refs.backdrop.addEventListener('click', closeModal);
-  window.addEventListener('keydown', onKeyDown);
   removeLoading();
 };
 
-refs.booksHandler.addEventListener('click', createModal);
+
+if (refs.booksHandler) {
+  refs.booksHandler.addEventListener('click', createModal);
+}
