bfddc8839f7d1d267df2f323369190db9d34fadb
GumeniukRuslan <bishob1997@gmail.com>
2023-05-16 10:01:51 +0300

    made pagination nice

--- src/js/helpers/deleteBook.js
+++ src/js/helpers/deleteBook.js
@@ -3,46 +3,77 @@ import { rmvFrmShopList } from '../firebase';
 import { getBksFrmShpLst } from '../firebase';
 import { renderError } from '../render/renderError';
 import { storagePaginationHolder } from '../createShopList';
+import { renderShopBookCards } from '../render/renderShopBookCard';
+import { removeLoading, startLoading } from './spinner';
+import { renderError } from '../render/renderError';
+import { getBksFrmShpLst } from '../firebase';
+import { createLibraryPagination } from '../render/renderPagination';
+import { handlePromiseArray } from '../createShopList';
+import { handleAuthStateChanged } from '../firebase';
 
 export async function deleteBook(evt) {
   if (!evt.target.closest('.delete-btn')) {
     return;
   }
+  startLoading();
   refs.shopList.removeChild(evt.target.closest('.shopping-list_book'));
   await rmvFrmShopList(evt);
-  const data = await getBksFrmShpLst();
-  if (!refs.shopList.children.length) {
-    if (!data.length) {
-      console.log(data);
-      refs.paginationBlock.style.display = 'none';
-      return (refs.shopList.innerHTML = renderError(
-        `This page is empty, add some books and proceed to order.`
-      ));
+  try {
+    let currentPage = storagePaginationHolder.getCurrentPage();
+    const user = await handleAuthStateChanged();
+    const data = await getBksFrmShpLst();
+    const arrOfBooks = await handlePromiseArray(data);
+    console.log();
+    if (!arrOfBooks.length) {
+      throw new Error();
     }
-    if (storagePaginationHolder._currentPage === 1) {
-      storagePaginationHolder.movePageTo(
-        storagePaginationHolder.getCurrentPage() + 1
-      );
-    } else if (
-      !(
-        storagePaginationHolder._currentPage <
-        storagePaginationHolder._options.visiblePages
-      )
-    ) {
-      storagePaginationHolder.movePageTo(
-        storagePaginationHolder.getCurrentPage() - 1
-      );
-    } else if (
-      storagePaginationHolder._currentPage <
-      storagePaginationHolder._options.visiblePages
-    ) {
-      storagePaginationHolder.movePageTo(
-        storagePaginationHolder.getCurrentPage() + 1
-      );
-
-      if (storagePaginationHolder._options.visiblePages === 1) {
-        refs.paginationBlock.style.display = 'none';
-      }
-    }
+    refs.shopList.innerHTML = renderShopBookCards(arrOfBooks.slice(0, 3));
+    let storagePaginationHolderAfter = createLibraryPagination(
+      arrOfBooks,
+      arrOfBooks.length
+    );
+    storagePaginationHolderAfter.movePageTo(currentPage);
+    currentPage = storagePaginationHolderAfter.getCurrentPage();
+    removeLoading();
+  } catch (e) {
+    refs.shopList.innerHTML = renderError(
+      `This page is empty, add some books and proceed to order.`
+    );
+    removeLoading();
   }
+
+  // if (!refs.shopList.children.length) {
+  //   if (!data.length) {
+  //     console.log(data);
+  //     refs.paginationBlock.style.display = 'none';
+  //     return (refs.shopList.innerHTML = renderError(
+  //       `This page is empty, add some books and proceed to order.`
+  //     ));
+  //   }
+  //   if (storagePaginationHolder._currentPage === 1) {
+  //     storagePaginationHolder.movePageTo(
+  //       storagePaginationHolder.getCurrentPage() + 1
+  //     );
+  //   } else if (
+  //     !(
+  //       storagePaginationHolder._currentPage <
+  //       storagePaginationHolder._options.visiblePages
+  //     )
+  //   ) {
+  //     storagePaginationHolder.movePageTo(
+  //       storagePaginationHolder.getCurrentPage() - 1
+  //     );
+  //   } else if (
+  //     storagePaginationHolder._currentPage <
+  //     storagePaginationHolder._options.visiblePages
+  //   ) {
+  //     storagePaginationHolder.movePageTo(
+  //       storagePaginationHolder.getCurrentPage() + 1
+  //     );
+
+  //     if (storagePaginationHolder._options.visiblePages === 1) {
+  //       refs.paginationBlock.style.display = 'none';
+  //     }
+  //   }
+  // }
 }
--- src/js/helpers/spinner.js
+++ src/js/helpers/spinner.js
@@ -1,13 +1,14 @@
-import { Loading } from "notiflix";
+import { Loading } from 'notiflix';
 
 const startLoading = () => {
-    Loading.pulse({
-        svgColor: '#4F2EE8'
-    })
-}
+  Loading.pulse({
+    svgColor: '#4F2EE8',
+    backgroundColor: 'rgba(0, 0, 0, 0.9)',
+  });
+};
 
 const removeLoading = () => {
-    Loading.remove();
-}
+  Loading.remove();
+};
 
-export {startLoading, removeLoading}
+export { startLoading, removeLoading };
