22a50b8697d0a3b08e655465289e05e9ec25fa0f
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-05-16 13:20:09 +0300

    update team info

--- src/js/components/team.js
+++ src/js/components/team.js
@@ -3,6 +3,10 @@ import PolishchukOlexandra from '../../images/PolishchukOlexandra.jpg';
 import VorobiovArtem from '../../images/VorobiovArtem.jpg';
 import ArturHlebov from '../../images/ArturHlebov.jpg';
 import OlegKiosa from '../../images/OlegKiosa.jpg';
+import NikitaVolkov from '../../images/NikitaVolkov.jpg';
+import AlexeuyKostiuk from '../../images/AlexeuyKostiuk.jpg';
+import MaximKruchko from '../../images/MaximKruchko.jpg';
+import MaksymManzherovskyi from '../../images/MaksymManzherovskyi.jpg';
 
 export const team = [
     {
@@ -30,8 +34,8 @@ export const team = [
         }],
     },
     {
-        name: 'Никита Волков',
-        photo: null,
+        name: 'Nikita Volkov',
+        photo: NikitaVolkov,
         position: 'Teamlead assistant',
         links: [{
             name: 'github',
@@ -78,12 +82,12 @@ export const team = [
         }],
     },
     {
-        name: 'Alexey',
-        photo: null,
+        name: 'Alexeuy Kostiuk',
+        photo: AlexeuyKostiuk,
         position: 'Developer',
         links: [{
             name: 'github',
-            url: null,
+            url: 'https://github.com/AlKostiuk',
         }, {
             name: 'linkedin',
             url: null,
@@ -91,14 +95,26 @@ export const team = [
     },
     {
         name: 'Maxim Kruchko',
-        photo: null,
+        photo: MaximKruchko,
         position: 'Developer',
         links: [{
             name: 'github',
             url: 'https://github.com/MaximSommelier',
         }, {
             name: 'linkedin',
-            url: null,
+            url: 'https://www.linkedin.com/in/maksym-kruchko-172903275/',
         }],
     },
-]
+    {
+        name: 'Maksym Manzherovskyi',
+        photo: MaksymManzherovskyi,
+        position: 'Developer',
+        links: [{
+            name: 'github',
+            url: 'https://github.com/MaxxMannn',
+        }, {
+            name: 'linkedin',
+            url: 'https://www.linkedin.com/in/max-m-23a37325b/',
+        }],
+    },
+];
