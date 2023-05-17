297e80400f15fe51b4609b6d1d399cafeb40ec64
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-05-17 15:41:25 +0300

    make active page for mobile menu

--- src/js/activePage.js
+++ src/js/activePage.js
@@ -2,14 +2,14 @@ import { refs } from "./components/refs"
 
 const activePageHighlight = () => {
     const titleOfPage = document.querySelector('title').textContent.toLowerCase();
-    const homeLink = document.querySelector('[data-page="home"]');
-    const shopLink = document.querySelector('[data-page="shop-list"]');
+    const homeLinks = document.querySelectorAll('[data-page="home"]');
+    const shopLinks = document.querySelectorAll('[data-page="shop-list"]');
     if (titleOfPage === 'bookshelf') {
-        homeLink.classList.add('active-page');
-        shopLink.classList.remove('active-page');
+        homeLinks.forEach(homeLink => homeLink.classList.add('active-page'));
+        shopLinks.forEach(shopLink => shopLink.classList.remove('active-page'));
     } else if (titleOfPage === 'shopping list') {
-        homeLink.classList.remove('active-page');
-        shopLink.classList.add('active-page');
+        homeLinks.forEach(homeLink => homeLink.classList.remove('active-page'));
+        shopLinks.forEach(shopLink => shopLink.classList.add('active-page'));
     } else {
         return;
     }
