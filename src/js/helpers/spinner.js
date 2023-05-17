8f065febb11585daca314cf1bb087626c06b45ee
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-05-17 20:37:39 +0300

    update modal styles

--- src/js/helpers/spinner.js
+++ src/js/helpers/spinner.js
@@ -11,4 +11,11 @@ const removeLoading = () => {
   Loading.remove();
 };
 
-export { startLoading, removeLoading };
+const LoaderBackdropStart = () => {
+  Loading.pulse({
+    svgColor: '#4F2EE8',
+    backgroundColor: 'rgba(17, 17, 17, 0.6)',
+  });
+}
+
+export { startLoading, removeLoading, LoaderBackdropStart };
