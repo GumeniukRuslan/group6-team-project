6d5b42e094b49de590d642868ec3b0eca87a476d
GumeniukRuslan <bishob1997@gmail.com>
2023-05-18 19:03:51 +0300

    minor upd

--- src/js/helpers/deleteAllBooks.js
+++ src/js/helpers/deleteAllBooks.js
@@ -1,14 +1,14 @@
 import { rmvAllFrmShopList } from '../firebase';
 import { removeLoading, startLoading } from './spinner';
 import { refs } from '../components/refs';
-import { renderError } from '../render/renderError';
+import { renderErrorShopList } from '../render/renderError';
 
 export async function deleteAllBooks() {
   startLoading();
   await rmvAllFrmShopList();
   refs.paginationBlock.style.display = 'none';
   refs.deleteAllBtn.classList.add('hidden');
-  refs.shopList.innerHTML = renderError(
+  refs.shopList.innerHTML = renderErrorShopList(
     `This page is empty, add some books and proceed to order.`
   );
   removeLoading();
