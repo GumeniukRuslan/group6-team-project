577906c40e2043b836d01452bb33720b79f8d0c4
GumeniukRuslan <bishob1997@gmail.com>
2023-05-18 11:48:51 +0300

    fix pagination bug

--- src/js/helpers/deleteBook.js
+++ src/js/helpers/deleteBook.js
@@ -10,6 +10,8 @@ import { getBksFrmShpLst } from '../firebase';
 import { createLibraryPagination } from '../render/renderPagination';
 import { handlePromiseArray } from '../createShopList';
 
+let storagePaginationHolderAfter;
+
 export async function deleteBook(evt) {
   if (!evt.target.closest('.delete-btn')) {
     return;
@@ -18,7 +20,13 @@ export async function deleteBook(evt) {
   refs.shopList.removeChild(evt.target.closest('.shopping-list_book'));
   await rmvFrmShopList(evt);
   try {
-    let currentPage = storagePaginationHolder.getCurrentPage();
+    let currentPage;
+    if (!storagePaginationHolderAfter) {
+      currentPage = storagePaginationHolder.getCurrentPage();
+    } else {
+      currentPage = storagePaginationHolderAfter.getCurrentPage();
+    }
+
     const data = await getBksFrmShpLst();
     const arrOfBooks = await handlePromiseArray(data);
     console.log();
@@ -26,7 +34,7 @@ export async function deleteBook(evt) {
       throw new Error();
     }
     refs.shopList.innerHTML = renderShopBookCards(arrOfBooks.slice(0, 3));
-    let storagePaginationHolderAfter = createLibraryPagination(
+    storagePaginationHolderAfter = createLibraryPagination(
       arrOfBooks,
       arrOfBooks.length
     );
